8fa21e71309e7dc447eda852fa7b4f11c0f555b6
Jayden Bailey <jaydenkieran@gmail.com>
2018-11-21 18:10:58 +0000

    Add config variables for all tweaks

--- WeirdGloopMessages.hooks.php
+++ WeirdGloopMessages.hooks.php
@@ -20,43 +20,46 @@ class WeirdGloopMessagesHooks {
 	 * @return bool
 	 */
 	public static function onMessageCacheGet( &$lcKey ) {
-		global $wgLanguageCode;
+		global $wglEnableMessageOverrides, $wgLanguageCode;
 
-		static $keys = [
-			'privacypage',
-			'changecontentmodel-text',
-			'emailmessage',
-			'mobile-frontend-copyright',
-			'contactpage-pagetext'
-		];
-
-		if ( in_array( $lcKey, $keys, true ) ) {
-			$transformedKey = "weirdgloop-$lcKey";
-		} else {
-			return true;
+		if ($wglEnableMessageOverrides) {
+			static $keys = [
+				'privacypage',
+				'changecontentmodel-text',
+				'emailmessage',
+				'mobile-frontend-copyright',
+				'contactpage-pagetext'
+			];
+	
+			if ( in_array( $lcKey, $keys, true ) ) {
+				$transformedKey = "weirdgloop-$lcKey";
+			} else {
+				return true;
+			}
+	
+			// MessageCache uses ucfirst if ord( key ) is < 128, which is true of all
+			// of the above.  Revisit if non-ASCII keys are used.
+			$ucKey = ucfirst( $lcKey );
+	
+			$cache = MessageCache::singleton();
+			if (
+				/*
+					* Override order:
+					* 1. If the MediaWiki:$ucKey page exists, use the key unprefixed
+					* (in all languages) with normal fallback order.  Specific
+					* language pages (MediaWiki:$ucKey/xy) are not checked when
+					* deciding which key to use, but are still used if applicable
+					* after the key is decided.
+					*
+					* 2. Otherwise, use the prefixed key with normal fallback order
+					* (including MediaWiki pages if they exist).
+					*/
+				$cache->getMsgFromNamespace( $ucKey, $wgLanguageCode ) === false
+			) {
+				$lcKey = $transformedKey;
+			}
 		}
 
-		// MessageCache uses ucfirst if ord( key ) is < 128, which is true of all
-		// of the above.  Revisit if non-ASCII keys are used.
-		$ucKey = ucfirst( $lcKey );
-
-		$cache = MessageCache::singleton();
-		if (
-			/*
-				* Override order:
-				* 1. If the MediaWiki:$ucKey page exists, use the key unprefixed
-				* (in all languages) with normal fallback order.  Specific
-				* language pages (MediaWiki:$ucKey/xy) are not checked when
-				* deciding which key to use, but are still used if applicable
-				* after the key is decided.
-				*
-				* 2. Otherwise, use the prefixed key with normal fallback order
-				* (including MediaWiki pages if they exist).
-				*/
-			$cache->getMsgFromNamespace( $ucKey, $wgLanguageCode ) === false
-		) {
-			$lcKey = $transformedKey;
-		}
 		return true;
 	}
 
@@ -71,8 +74,12 @@ class WeirdGloopMessagesHooks {
 	 * @return bool
 	 */
 	public static function onSkinCopyrightFooter( $title, $type, &$msg, &$link ) {
-		if ( $type !== 'history' ) {
-			$msg = 'weirdgloop-copyright';
+		global $wglEnableMessageOverrides;
+
+		if ($wglEnableMessageOverrides) {
+			if ( $type !== 'history' ) {
+				$msg = 'weirdgloop-copyright';
+			}
 		}
 
 		return true;
@@ -87,15 +94,20 @@ class WeirdGloopMessagesHooks {
 	 * @return bool
 	 */
 	public static function onSkinTemplateOutputPageBeforeExec( &$skin, &$template ) {
-		$touDest = Skin::makeInternalOrExternalUrl(
-			$skin->msg( 'weirdgloop-tou-url' )->inContentLanguage()->text() );
-		$touLink = Html::element(
-			'a',
-			[ 'href' => $touDest ],
-			$skin->msg( 'weirdgloop-tou' )->text()
-		);
-		$template->set( 'tou', $touLink );
-		$template->data['footerlinks']['places'][] = 'tou';
+		global $wglAddFooterLinks;
+
+		if ($wglAddFooterLinks) {
+			$touDest = Skin::makeInternalOrExternalUrl(
+				$skin->msg( 'weirdgloop-tou-url' )->inContentLanguage()->text() );
+			$touLink = Html::element(
+				'a',
+				[ 'href' => $touDest ],
+				$skin->msg( 'weirdgloop-tou' )->text()
+			);
+			$template->set( 'tou', $touLink );
+			$template->data['footerlinks']['places'][] = 'tou';
+		}
+
 		return true;
 	}
 
@@ -107,7 +119,12 @@ class WeirdGloopMessagesHooks {
 	 * @return bool
 	 */
 	public static function onTorBlockBlockedMsg( &$msg ) {
-		$msg = 'weirdgloop-torblock-blocked';
+		global $wglEnableMessageOverrides;
+
+		if ($wglEnableMessageOverrides) {
+			$msg = 'weirdgloop-torblock-blocked';
+		}
+
 		return true;
 	}
 
@@ -119,7 +136,12 @@ class WeirdGloopMessagesHooks {
 	 * @return bool
 	 */
 	public static function onGlobalBlockingBlockedIpMsg( &$msg ) {
-		$msg = 'weirdgloop-globalblocking-ipblocked';
+		global $wglEnableMessageOverrides;
+
+		if ($wglEnableMessageOverrides) {
+			$msg = 'weirdgloop-globalblocking-ipblocked';
+		}
+
 		return true;
 	}
 
@@ -131,7 +153,12 @@ class WeirdGloopMessagesHooks {
 	 * @return bool
 	 */
 	public static function onGlobalBlockingBlockedIpXffMsg( &$msg ) {
-		$msg = 'weirdgloop-globalblocking-ipblocked-xff';
+		global $wglEnableMessageOverrides;
+
+		if ($wglEnableMessageOverrides) {
+			$msg = 'weirdgloop-globalblocking-ipblocked-xff';
+		}
+
 		return true;
 	}
 
@@ -146,12 +173,16 @@ class WeirdGloopMessagesHooks {
 	 * @throws ErrorPageError
 	 */
 	public static function onUploadFormInitial( $tpl ) {
-		global $wgForceUIMsgAsContentMsg;
-		if ( !in_array( 'licenses', $wgForceUIMsgAsContentMsg )
-			&& wfMessage( 'licenses' )->inContentLanguage()->isDisabled()
-		) {
-			throw new ErrorPageError( 'uploaddisabled', 'weirdgloop-upload-nolicenses' );
+		global $wglRequireLicensesToUpload, $wgForceUIMsgAsContentMsg;
+		
+		if ($wglRequireLicensesToUpload) {
+			if ( !in_array( 'licenses', $wgForceUIMsgAsContentMsg )
+				&& wfMessage( 'licenses' )->inContentLanguage()->isDisabled()
+			) {
+				throw new ErrorPageError( 'uploaddisabled', 'weirdgloop-upload-nolicenses' );
+			}
 		}
+
 		return true;
 	}
 
@@ -167,7 +198,7 @@ class WeirdGloopMessagesHooks {
 	public static function ongetUserPermissionsErrors( $title, $user, $action, &$result ) {
 		global $wglProtectSiteInterface;
 
-		if ( $wglProtectSiteInterface === true
+		if ( $wglProtectSiteInterface
 			&& $title->inNamespace( NS_MEDIAWIKI )
 			&& strpos( lcfirst( $title->getDBKey() ), 'weirdgloop-' ) === 0
 			&& !$user->isAllowed( 'editinterfacesite' )
@@ -183,38 +214,48 @@ class WeirdGloopMessagesHooks {
 	 * Implement a dark mode and add structured data for the Google Sitelinks search box.
 	 */
 	public static function onBeforePageDisplay( OutputPage &$out, Skin &$skin ) {
-		global $wgArticlePath, $wgCanonicalServer;
+		global $wglEnableLoadingDarkmode, $wglEnableSearchboxMetadata, $wgArticlePath, $wgCanonicalServer;
 
-		/* Dark mode */
-		if ( isset( $_COOKIE['darkmode'] ) ) {
-			if ( $_COOKIE['darkmode'] == 'true' ) {
-				$out->addModuleStyles( [ 'wg.darkmode' ] );
+		if ($wglEnableLoadingDarkmode) {
+			/* Dark mode */
+			if ( isset( $_COOKIE['darkmode'] ) ) {
+				if ( $_COOKIE['darkmode'] == 'true' ) {
+					$out->addModuleStyles( [ 'wg.darkmode' ] );
+				}
 			}
 		}
 
-		/* Structured data for the Google Sitelinks search box. */
-		if ( $out->getTitle()->isMainPage() ) {
-			$targetUrl = $wgCanonicalServer . str_replace( '$1', 'Special:Search', $wgArticlePath );
-			$targetUrl = wfAppendQuery( $targetUrl, 'search={search_term_string}' );
-			$structuredData = [
-				'@context'        => 'http://schema.org',
-				'@type'           => 'WebSite',
-				'url'             => $wgCanonicalServer,
-				'potentialAction' => [
-					'@type'       => 'SearchAction',
-					'target'      => $targetUrl,
-					'query-input' => 'required name=search_term_string',
-				],
-			];
-			$out->addHeadItem( 'StructuredData', '<script type="application/ld+json">' . json_encode( $structuredData ) . '</script>' );
+		if ($wglEnableSearchboxMetadata) {
+			/* Structured data for the Google Sitelinks search box. */
+			if ( $out->getTitle()->isMainPage() ) {
+				$targetUrl = $wgCanonicalServer . str_replace( '$1', 'Special:Search', $wgArticlePath );
+				$targetUrl = wfAppendQuery( $targetUrl, 'search={search_term_string}' );
+				$structuredData = [
+					'@context'        => 'http://schema.org',
+					'@type'           => 'WebSite',
+					'url'             => $wgCanonicalServer,
+					'potentialAction' => [
+						'@type'       => 'SearchAction',
+						'target'      => $targetUrl,
+						'query-input' => 'required name=search_term_string',
+					],
+				];
+				$out->addHeadItem( 'StructuredData', '<script type="application/ld+json">' . json_encode( $structuredData ) . '</script>' );
+			}
 		}
+
+		return true;
 	}
 
 	public static function onOutputPageBodyAttributes( OutputPage $out, Skin $sk, &$bodyAttrs ) {
-		if ( isset( $_COOKIE['darkmode'] ) ) {
-			if ( $_COOKIE['darkmode'] == 'true' ) {
-				// Add a class to the body so that gadgets can identify that we're using darkmode
-				$bodyAttrs['class'] .= ' wgl-darkmode';
+		global $wglEnableLoadingDarkmode;
+
+		if ($wglEnableLoadingDarkmode) {
+			if ( isset( $_COOKIE['darkmode'] ) ) {
+				if ( $_COOKIE['darkmode'] == 'true' ) {
+					// Add a class to the body so that gadgets can identify that we're using darkmode
+					$bodyAttrs['class'] .= ' wgl-darkmode';
+				}
 			}
 		}
 
@@ -226,16 +267,18 @@ class WeirdGloopMessagesHooks {
 	 * Hook provided by ContactPage extension.
 	 */
 	public static function onContactPage( &$to, &$replyTo, &$subject, &$text ) {
-		global $wgDBname, $wgRequest, $wgOut, $wgServer;
+		global $wglSendDetailsWithContactPage, $wgDBname, $wgRequest, $wgOut, $wgServer;
 
-		$user = $wgOut->getUser();
+		if ($wglSendDetailsWithContactPage) {
+			$user = $wgOut->getUser();
 
-		$text = $wgRequest->getText( 'wpText' ) . "\n\n---\n\n"; // original message
-		$text .= $wgServer . ' (' . $wgDBname . ") [" . gethostname() . "]\n"; // server & database name
-		$text .= $wgRequest->getIP() . ' - ' . ( $_SERVER['HTTP_USER_AGENT'] ?? null ) . "\n"; // IP & user agent
-		$text .= 'Referrer: ' . ( $_SERVER['HTTP_REFERER'] ?? null ) . "\n"; // referrer if any
-		$text .= 'Skin: ' . $wgOut->getSkin()->getSkinName() . "\n"; // skin
-		$text .= 'User: ' . $user->getName() . ' (' . $user->getId() . ')'; // user
+			$text = $wgRequest->getText( 'wpText' ) . "\n\n---\n\n"; // original message
+			$text .= $wgServer . ' (' . $wgDBname . ") [" . gethostname() . "]\n"; // server & database name
+			$text .= $wgRequest->getIP() . ' - ' . ( $_SERVER['HTTP_USER_AGENT'] ?? null ) . "\n"; // IP & user agent
+			$text .= 'Referrer: ' . ( $_SERVER['HTTP_REFERER'] ?? null ) . "\n"; // referrer if any
+			$text .= 'Skin: ' . $wgOut->getSkin()->getSkinName() . "\n"; // skin
+			$text .= 'User: ' . $user->getName() . ' (' . $user->getId() . ')'; // user
+		}
 
 		return true;
 	}
