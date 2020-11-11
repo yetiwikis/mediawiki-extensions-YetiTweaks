612b3bb9275fdb8c9bfaeeaa2b344f508cf0b25b
TehKittyCat <tehkittycat@weirdgloop.org>
2020-11-10 23:09:29 -0500

    Add maintenance script to generate the GitInfo cache. It's intended for use in the build process, resulting in a much faster Special:Version and eliminates shelling out to git.

---
+++ maintenance/generateGitInfoCache.php
@@ -0,0 +1,36 @@
+<?php
+if ( getenv( 'MW_INSTALL_PATH' ) ) {
+	$IP = getenv( 'MW_INSTALL_PATH' );
+} else {
+	$IP = __DIR__ . '/../../..';
+}
+require_once "$IP/maintenance/Maintenance.php";
+
+class GenerateGitInfoCache extends Maintenance {
+	public function execute() {
+        global $IP;
+
+        $this->output( "Generating GitInfo cache...\n" );
+
+        $patterns = [
+            "$IP",
+            "$IP/extensions/*",
+            "$IP/skins/*",
+        ];
+
+        foreach ($patterns as $pattern) {
+            $directories = glob($pattern);
+
+            foreach ($directories as $directory) {
+                if (is_dir($directory)) {
+                    $this->output( "Generating GitInfo cache for '$directory'.\n" );
+                    (new GitInfo( $directory ))->precomputeValues();
+                }
+            }
+        }
+	}
+}
+
+$maintClass = GenerateGitInfoCache::class;
+
+require_once RUN_MAINTENANCE_IF_MAIN;
