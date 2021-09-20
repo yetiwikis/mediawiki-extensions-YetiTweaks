e94df7cb5a404e7b90de9d4503490b331ee0cfcd
TehKittyCat <tehkittycat@weirdgloop.org>
2021-09-20 00:36:40 -0400

    Replace MediaWiki core changes for Cloudflare purging with a custom EventRelayer.

---
+++ includes/GloopEventRelayer.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * EventRelayer to perform Cloudflare purging.
+ * Note: This performs purges directly, so if purging fails for any reason, the purges are lost.
+ *
+ */
+class GloopEventRelayer extends EventRelayer {
+	// Cloudflare limits purge_cache API to 30 URLs per request.
+	private const MAX_URLS_PER_REQUEST = 30;
+
+	public function doNotify( $channel, array $events ) {
+		// This EventRelayer is for CDN URL purges only.
+		if ( $channel != 'cdn-url-purges' ) {
+			return false;
+		}
+
+		// Extract the URLs to purge from the 'cdn-url-purges' events.
+		$urls = [];
+		foreach ( $events as $event ) {
+			$urls[] = $event['url'];
+		}
+
+		// Purge the URLs from Cloudflare.
+		if ( count( $urls ) > 0 ) {
+			// MediaWiki doesn't guarantee the event URLs are unique, so avoid pointless purges since we're ignoring timestamp.
+			$urls = array_unique( $urls );
+
+			self::CloudflarePurge( $urls );
+		}
+
+		return true;
+	}
+
+	/**
+	* Send CloudFlare purge requests.
+	*
+	* @param string[] $urls Array of URLs to purge.
+	*/
+	private static function CloudflarePurge( array $urls ) {
+		global $wglCloudflareToken, $wglCloudflareZone;
+
+		// Break the purge requests into chunks sized to Cloudflare's per-request URL limit.
+		$chunks = array_chunk( $urls, self::MAX_URLS_PER_REQUEST );
+
+		// Prepare cURL
+		$ch = curl_init();
+		curl_setopt( $ch, CURLOPT_HTTPHEADER, [
+			"Authorization: Bearer $wglCloudflareToken",
+			'Content-Type: application/json'
+		] );
+		curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+		curl_setopt( $ch, CURLOPT_CONNECTTIMEOUT, 10 );
+		curl_setopt( $ch, CURLOPT_TIMEOUT, 10 );
+		curl_setopt( $ch, CURLOPT_URL, "https://api.cloudflare.com/client/v4/zones/$wglCloudflareZone/purge_cache" );
+
+		// Perform the purge requests a chunk at a time.
+		foreach ( $chunks as $chunk ) {
+			curl_setopt( $ch, CURLOPT_POSTFIELDS, '{"files":' . json_encode( $chunk, JSON_UNESCAPED_SLASHES ) . '}' );
+			curl_exec( $ch );
+		}
+		curl_close( $ch );
+	}
+}
