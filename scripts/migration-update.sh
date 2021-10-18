4e5389016b14e7777e7fe00ba7d1de5739cbeb75
TehKittyCat <tehkittycat@weirdgloop.org>
2021-10-18 00:12:51 -0400

    Typo.

--- scripts/migration-update.sh
+++ scripts/migration-update.sh
@@ -3,7 +3,7 @@
 cd /srv/mediawiki
 echo Updating $1...
 php extensions/GloopTweaks/maintenance/cleanupUpdatelog.php --wiki $1
-if [ "$1" -eq 'metawiki' ]; then
+if [ "$1" == 'metawiki' ]; then
     php maintenance/update.php --quick --skip-optimize --wiki metawiki --doshared
 else
     php maintenance/update.php --quick --skip-optimize --wiki $1
