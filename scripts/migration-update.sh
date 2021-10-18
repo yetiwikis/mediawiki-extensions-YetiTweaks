ca1ce165437ac27df3d7ece909b520c4ae09b77e
TehKittyCat <tehkittycat@weirdgloop.org>
2021-10-17 23:42:07 -0400

    Add migration update scripts.

---
+++ scripts/migration-update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd /srv/mediawiki
+echo Updating $1...
+php extensions/GloopTweaks/maintenance/cleanupUpdatelog.php --wiki $1
+if [ "$1" -eq 'metawiki' ]; then
+    php maintenance/update.php --quick --skip-optimize --wiki metawiki --doshared
+else
+    php maintenance/update.php --quick --skip-optimize --wiki $1
+fi
+php maintenance/migrateRevisionActorTemp.php --wiki $1
---
+++ scripts/parallel-migration-update.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+WIKIS="en_osrswiki en_rscwiki en_rswiki pt_rswiki"
+
+cd /srv/mediawiki
+echo Updating metawiki...
+php extensions/GloopTweaks/maintenance/cleanupUpdatelog.php --wiki metawiki
+php maintenance/update.php --wiki metawiki --quick --skip-optimize --doshared
+php maintenance/migrateRevisionActorTemp.php --wiki metawiki
+parallel --progress -j4 "echo Updating {}...; php extensions/GloopTweaks/maintenance/cleanupUpdatelog.php --wiki {}; php maintenance/update.php --wiki {} --quick --skip-optimize; php maintenance/migrateRevisionActorTemp.php --wiki {}" ::: $WIKIS
