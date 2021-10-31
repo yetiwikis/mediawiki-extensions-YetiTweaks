82129ae243838d58a905b10ea7473f1c90acf651
TehKittyCat <tehkittycat@weirdgloop.org>
2021-10-31 03:09:23 -0400

    Re-implement readermode as a global ResourceLoader module, located at 'MediaWiki:Vector-readermode.css' on metawiki.

---
+++ includes/modules/ReadermodeStyleModule.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace GloopTweaks\ResourceLoaderModules;
+
+use ResourceLoaderSiteStylesModule;
+use ResourceLoaderContext;
+
+class ReadermodeStyleModule extends ResourceLoaderSiteStylesModule {
+	// Readermode only makes sense for desktop.
+	protected $targets = [ 'desktop' ];
+
+	// Override getDB() to use metawiki rather than having a per-wiki MediaWiki:Vector-readermode.css.
+	protected function getDB() {
+		global $wglCentralDB;
+		$lbFactory = MediaWikiServices::getInstance()->getDBLoadBalancerFactory();
+		$lb = $lbFactory->getMainLB( $wglCentralDB );
+		return $lb->getLazyConnectionRef( DB_REPLICA, [], $wglCentralDB );
+	}
+
+	/**
+	 * Get list of pages used by this module
+	 *
+	 * @param ResourceLoaderContext $context
+	 * @return array[]
+	 */
+	protected function getPages( ResourceLoaderContext $context ) {
+		$pages = [];
+		if ( $this->getConfig()->get( 'UseSiteCss' ) ) {
+			$skin = $context->getSkin();
+			$pages['MediaWiki:' . ucfirst( $skin ) . '-readermode.css'] = [ 'type' => 'style' ];
+		}
+		return $pages;
+	}
+
+	// This is considered a foreign module, except on metawiki, since it's fetching from metawiki.
+	public function getSource() {
+		global $wglCentralDB, $wgDBname;
+		return ( $wgDBname === $wglCentralDB ) ? 'local' : $wglCentralDB;
+	}
+}
