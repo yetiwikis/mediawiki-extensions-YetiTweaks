4263f562118a632c990c476fdffbfee58b5f41ba
TehKittyCat <tehkittycat@weirdgloop.org>
2022-08-24 12:44:54 -0400

    Fix message overrides.

--- includes/GloopTweaksHooks.php
+++ includes/GloopTweaksHooks.php
@@ -46,31 +46,7 @@ class GloopTweaksHooks {
 			];
 
 			if ( in_array( $lcKey, $keys, true ) ) {
-				$transformedKey = "weirdgloop-$lcKey";
-			} else {
-				return;
-			}
-
-			// MessageCache uses ucfirst if ord( key ) is < 128, which is true of all
-			// of the above.  Revisit if non-ASCII keys are used.
-			$ucKey = ucfirst( $lcKey );
-
-			$cache = MediaWikiServices::getInstance()->getMessageCache();
-			if (
-				/*
-					* Override order:
-					* 1. If the MediaWiki:$ucKey page exists, use the key unprefixed
-					* (in all languages) with normal fallback order.  Specific
-					* language pages (MediaWiki:$ucKey/xy) are not checked when
-					* deciding which key to use, but are still used if applicable
-					* after the key is decided.
-					*
-					* 2. Otherwise, use the prefixed key with normal fallback order
-					* (including MediaWiki pages if they exist).
-					*/
-				$cache->getMsgFromNamespace( $ucKey, $wgLanguageCode ) === false
-			) {
-				$lcKey = $transformedKey;
+				$lcKey = "weirdgloop-$lcKey";
 			}
 		}
 	}
