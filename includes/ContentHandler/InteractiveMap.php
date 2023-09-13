741f9ea237d467056ad8fc355f317b2d997c1c42
Jayden Bailey <jaydenkieran@gmail.com>
2023-09-13 02:00:24 +0100

    Change InteractiveMap to TextContentHandler
    The Fandom XML dump for Minecraft appears to see InteractiveMap pages as text/plain, which can't be used with JsonContentHandler

--- includes/ContentHandler/InteractiveMap.php
+++ includes/ContentHandler/InteractiveMap.php
@@ -2,7 +2,7 @@
 
 namespace MediaWiki\Extension\GloopTweaks\ContentHandler;
 
-class InteractiveMap extends \JsonContentHandler {
+class InteractiveMap extends \TextContentHandler {
     public const CONTENT_MODEL_ID = 'interactivemap';
 
 	public function __construct( $modelId = self::CONTENT_MODEL_ID ) {
